2713c9ff8b2b175fc15b6d62b91c1cc488f4a49d
Kyle Bentle <kylebentle@gmail.com>
2017-05-18 14:35:10 -0500

    Refactors javascipt to use vanilla js instead of jquery, avoiding issues when map loads in p2p. Adds thumbnail.

--- js/src/app.js
+++ js/src/app.js
@@ -29,22 +29,59 @@ L.tileLayer(
   "http://media.apps.chicagotribune.com/maptiles/chicago-mask/{z}/{x}/{y}.png",
   { maxZoom: 16, minZoom: 9, opacity: 0.5 }).addTo(map);
 
+//FOLLOWING CODE USES JQUERY TO LOAD GEOJSON
 //load GeoJSON from an external file using jquery
-$.getJSON("data/commareas.geojson",function(hoodData){
-	L.geoJson( hoodData, {
-		style: commStyle
-	}).addTo(map);
-});
+// $.getJSON("data/commareas.geojson",function(hoodData){
+// 	L.geoJson( hoodData, {
+// 		style: commStyle
+// 	}).addTo(map);
+// });
 
-$.getJSON("data/locations.geojson",function(data){
-	// add GeoJSON layer to the map once the file is loaded
-	L.geoJson(data,{
-	      pointToLayer: function(feature,latlng){
-	        var marker = L.marker(latlng,{icon: myIcon});
-	        marker.bindPopup(feature.properties.Location + '<br/>' + feature.properties.Date);
-	        return marker;
-	      }
-	}).addTo(map);
-});
+// $.getJSON("data/locations.geojson",function(data){
+// 	// add GeoJSON layer to the map once the file is loaded
+// 	L.geoJson(data,{
+// 	      pointToLayer: function(feature,latlng){
+// 	        var marker = L.marker(latlng,{icon: myIcon});
+// 	        marker.bindPopup(feature.properties.Location + '<br/>' + feature.properties.Date);
+// 	        return marker;
+// 	      }
+// 	}).addTo(map);
+// });
+
+//THIS CODE USES VANILLA JAVASCRIPT TO LOAD THE GEOJSON
+function getGeoJson(url){
+  var request = new XMLHttpRequest();
+  request.open('GET', url, true);
+
+  request.onload = function() {
+    if (request.status >= 200 && request.status < 400) {
+      // Success!
+      var data = JSON.parse(request.responseText);
+      L.geoJson(data,{
+        pointToLayer: function(feature,latlng){
+          var marker = L.marker(latlng,{icon: myIcon});
+          marker.bindPopup(feature.properties.Location + '<br/>' + feature.properties.Date);
+          return marker;
+        }
+      }).addTo(map);
+    } else {
+      // We reached our target server, but it returned an error
+    }
+  };
+
+  request.onerror = function() {
+    // There was a connection error of some sort
+  };
+
+  request.send();
+};
+
+//THIS WORKS IN BETA NOT IN P2P
+//getGeoJson('data/locations.geojson');
+//getGeoJson('data/commareas.geojson');
+
+
+getGeoJson("http://" + window.ROOT_URL + "/data/locations.geojson");
+getGeoJson('http://' + window.ROOT_URL + '/data/commareas.geojson');
 
 
