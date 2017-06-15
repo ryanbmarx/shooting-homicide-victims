e39186d9d3400bbb08b960af7d169a1c9a3a970e
ryanbmarx <ryanbmarx@gmail.com>
2017-06-15 17:03:06 -0500

    Append SVG, get some housekeeping functioning

--- js/src/multiline-chart.js
+++ js/src/multiline-chart.js
@@ -1,7 +1,57 @@
-
+import * as d3 from 'd3';
 
 class MultilineChart{
 	constructor(options){
+		console.log(options)
+		const 	app = this;
+		app.options = options;
+		app.data = options.data;
+		app._container = options.container;
+
+		MultilineChart.initChart(app);
+	}
+
+
+	static initChart(app){
+		
+		// ----------------------------------
+		// GET THE KNOW THE CONTAINER
+		// ----------------------------------
+
+		const 	data = app.data,
+				container = d3.select(app._container),
+				bbox = app._container.getBoundingClientRect(), 
+				height = bbox.height,
+				width = bbox.width,
+				margin = app.options.innerMargins,
+				innerHeight = height - margin.top - margin.bottom,
+				innerWidth = width - margin.right - margin.left;
+
+
+
+
+
+
+		// ----------------------------------
+		// start working with the SVG
+		// ----------------------------------
+
+		const svg = container
+			.append('svg')
+			.attr('width', width)
+			.attr('height', height);
+
+		const chartInner = svg
+			.append('g')
+			.classed('chart-inner', true)
+			.attr('width', innerWidth)
+			.attr('height', innerHeight)
+			.attr('transform', `translate(${margin.left}, ${margin.top})`);
+
+		// ----------------------------------
+		// APPEND AXES
+		// ----------------------------------
+
 
 	}
 }
