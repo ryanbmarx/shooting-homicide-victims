f67d44ed4439e119654715f59329c2ba29c2183a
ryanbmarx <ryanbmarx@gmail.com>
2017-06-21 18:29:09 -0500

    January = 0

--- js/src/month-formatter.js
+++ js/src/month-formatter.js
@@ -3,18 +3,18 @@ function monthFormatter(month){
 	// takes a month as number and returns AP style abbreviation. If the window width is too small, then it returns an even shorter version.
 	
 	const months = {
-		1:{ap: "Jan.", short:"J"},
-		2:{ap: "Feb.", short:"F"},
-		3:{ap: "March", short:"M"},
-		4:{ap: "April", short:"A"},
-		5:{ap: "May", short:"M"},
-		6:{ap: "June", short:"J"},
-		7:{ap: "July", short:"J"},
-		8:{ap: "Aug.", short:"A"},
-		9:{ap: "Sept.", short:"S"},
-		10:{ap: "Oct.", short:"O"},
-		11:{ap: "Nov.", short:"N"},
-		12:{ap: "Dec.", short:"D"}
+		0:{ap: "Jan.", short:"J"},
+		1:{ap: "Feb.", short:"F"},
+		2:{ap: "March", short:"M"},
+		3:{ap: "April", short:"A"},
+		4:{ap: "May", short:"M"},
+		5:{ap: "June", short:"J"},
+		6:{ap: "July", short:"J"},
+		7:{ap: "Aug.", short:"A"},
+		8:{ap: "Sept.", short:"S"},
+		9:{ap: "Oct.", short:"O"},
+		10:{ap: "Nov.", short:"N"},
+		11:{ap: "Dec.", short:"D"}
 	}
 
 	try{
