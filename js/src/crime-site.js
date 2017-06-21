0f992e968e417d384d26a3e4bc708eba11b16020
ryanbmarx <ryanbmarx@gmail.com>
2017-06-21 12:16:41 -0500

    Numbers in CSV
    The numbers are coming across as strings. They should be numbers, so I added a little step immediately after downloading which coerces those strings into numbers.

--- js/src/crime-site.js
+++ js/src/crime-site.js
@@ -94,7 +94,11 @@ class CrimeSite{
 
 			})
 
-			csv(`http://${ app.options.ROOT_URL }/frisco_brisco/shootings_upUntil_2016.csv`, (err, dailyData) => {
+			csv(`http://${ app.options.ROOT_URL }/frisco_brisco/shootings_upUntil_2016.csv`,function(d, i, columns) {
+  				// This coerces the data into numbers
+  				for (var i = 1, n = columns.length; i < n; ++i) d[columns[i]] = +d[columns[i]];
+	  				return d;
+				}, (err, dailyData) => {
 				
 
 				const cumulativeChart = new MultilineChart({
