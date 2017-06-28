d17a2f5feb7bd8bcf75a888b582dfb8c221f4ca2
ryanbmarx <ryanbmarx@gmail.com>
2017-06-28 13:21:41 -0500

    Use waypoints to zoom around map, merged with list

--- js/src/shootings-map.js
+++ js/src/shootings-map.js
@@ -1,6 +1,7 @@
 import * as L from 'leaflet';
 import "leaflet-providers";
 import {timeParse, timeFormat} from 'd3';
+require('waypoints/lib/noframework.waypoints.min');
 
 
 // function getShootingMarkerIcon(shooting){
@@ -42,7 +43,7 @@ class ShootingsMap{
 
 		//SETS UP MAP
 
-		const map =  L.map(container,{
+		app.map =  L.map(container,{
 			center: [41.838299, -87.706953],
 			zoom: 11,
 			scrollWheelZoom:false,
@@ -54,14 +55,14 @@ class ShootingsMap{
 		// This is using an npm plugin. Can be adjusted for many map types.
 		// https://leaflet-extras.github.io/leaflet-providers/preview/
 
-		L.tileLayer.provider('Stamen.TonerBackground').addTo(map);
+		L.tileLayer.provider('Stamen.TonerBackground').addTo(app.map);
 
 		//ADDS CITY MASK
 		L.tileLayer( "http://media.apps.chicagotribune.com/maptiles/chicago-mask/{z}/{x}/{y}.png", { 
 			maxZoom: 16, 
 			minZoom: 10, 
 			opacity: 0.5 
-		}).addTo(map);
+		}).addTo(app.map);
 
 		app.fatalShootingMarkers = L.layerGroup();
 		app.nonFatalShootingMarkers = L.layerGroup();
@@ -94,11 +95,35 @@ class ShootingsMap{
 				
 			}
 		})
-		app.nonFatalShootingMarkers.addTo(map);
-		app.fatalShootingMarkers.addTo(map);
-		app.highlightedShootings.addTo(map)
+		app.nonFatalShootingMarkers.addTo(app.map);
+		app.fatalShootingMarkers.addTo(app.map);
+		app.highlightedShootings.addTo(app.map)
 
-		app.highlightShooting(16856);
+		// app.highlightShooting(16856);
+
+		// init the highlight event listener
+		const victims = options.victimList.querySelectorAll('ul li');
+		
+		for (let i = 0; i < victims.length; i++) {
+			const victim = victims[i];
+			new Waypoint({
+				element: victim,
+				handler: function(direction){
+					const shootingID = this.element.dataset.shootingId;
+					app.highlightShooting(shootingID);
+					console.log(direction, this, this.element);
+
+					// Toggle highlight classes on the list, so the current one is shown.
+					const highlightedVictim = document.querySelector('li.victim.victim--highlight');
+					if (highlightedVictim != null) highlightedVictim.classList.remove('victim--highlight');
+					this.element.classList.add('victim--highlight');
+
+				},
+				context: options.victimList,
+				offset: "200px"
+
+			});
+		}
 	}
 
 	highlightShooting(shootingID){
@@ -125,7 +150,7 @@ class ShootingsMap{
 					fill: true,
 					fillOpacity: .8
 				}).addTo(app.highlightedShootings);
-				
+				app.map.panTo(l.getLatLng());
 			};
 		})
 	}
