377869416ef033c190340af6b61cf273b5aa5c1b
ryanbmarx <ryanbmarx@gmail.com>
2017-08-19 19:36:07 -0500

    Make new set of build scripts

--- js/src/crime-site.js
+++ js/src/crime-site.js
@@ -175,7 +175,7 @@ class CrimeSite{
 		const cumulativeChart = new MultilineChart({
 			container: document.querySelector('#cumulative'),
 			data: groupBy(data, d=> d['YEAR']), // seperate the rows into year groups
-	        innerMargins:{ top:7,right:0,bottom:20,left:50 },
+	        innerMargins:{ top:7,right:0,bottom:20,left:40 },
 			currentColor: app.options.currentColor,
 			otherColor: app.options.otherColor
 		});
