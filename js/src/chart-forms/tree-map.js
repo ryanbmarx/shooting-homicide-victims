ffdbd10dcc58c34d6ea9efd377ddd562873b9ee5
ryanbmarx <ryanbmarx@gmail.com>
2017-08-23 13:08:46 -0500

    change color pallete

--- js/src/chart-forms/tree-map.js
+++ js/src/chart-forms/tree-map.js
@@ -18,15 +18,14 @@ class TreeMap{
 				innerHeight = height - margin.top - margin.bottom,
 				innerWidth = width - margin.right - margin.left,
 				colors = [
-					getTribColor('trib_red2'),
-					getTribColor('trib_orange'),
-					getTribColor('trib_yellow1'),
 					getTribColor('trib_blue2'),
 					getTribColor('trib_blue3'),
 					getTribColor('trib_blue5'),
+					getTribColor('trib_blue_gray'),
+					getTribColor('trib_blue1'),
+					getTribColor('trib_blue4'),
 					getTribColor('trib_green2'),
 					getTribColor('trib_green4'),
-					getTribColor('trib_blue_gray')
 				];
 		
 		const colorScale = d3.scaleOrdinal()
