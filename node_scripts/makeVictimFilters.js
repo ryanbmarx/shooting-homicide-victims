c804e0ccabf6e2773b4af3c6d808474b241615f1
ryanbmarx <ryanbmarx@gmail.com>
2017-08-25 13:53:48 -0500

    make labels more better formatted on the buttons

--- node_scripts/makeVictimFilters.js
+++ node_scripts/makeVictimFilters.js
@@ -18,10 +18,102 @@ function getButtonString(category, victimsArray){
 
 	orderBy(victimsArray, v => v[category] == "" ? "Unknown" : v[category]).forEach(v => {
 		let label = v[category] == "" ? "Unknown" : v[category];
-		retval += `<button class='filter-button' data-checked=true data-cat='${category.toLowerCase()}' data-cat-value='${label.toLowerCase()}'>${label.toLowerCase()}</button>`;
+		retval += `<button class='filter-button' data-checked=true data-cat='${category.toLowerCase()}' data-cat-value='${label.toLowerCase()}'>${labelFormatters[category](label)}</button>`;
 	})
 
 	return retval;
+}
+
+const labelFormatters = {};
+
+labelFormatters.MAIN = function(att){
+	// Takes attribute and returns human readable version for labeling purposes
+	switch (att){
+		case "PUB_CAUSE":
+			return "Cause of death";
+			break;
+		case "RACE":
+			return "Race";
+			break;
+		case "SEX":
+			return "Sex";
+			break;
+	}
+	return att;
+}
+
+labelFormatters.SEX = function(att){
+		switch (att){
+		case "M":
+			return "Male";
+			break;
+		case "F":
+			return "Female";
+			break;
+		case "UNKNOWN":
+			return "Unknown";
+			break;
+	}
+	return att;
+}
+
+labelFormatters.RACE = function(att){
+		switch (att){
+		case "B":
+			return "Black, not hispanic";
+			break;
+		case "B(H)":
+			return "Black, hispanic";
+			break;
+		case "W":
+			return "White, not hispanic";
+			break;
+		case "W(H)":
+			return "White, hispanic";
+			break;
+		case "UNKNOWN":
+			return "Unknown";
+			break;
+
+	}
+	return att;
+}
+
+labelFormatters.PUB_CAUSE = function(att){
+	switch (att.toUpperCase()){		
+		case "ASSAULT":
+			return "Assault";
+			break;
+		case "BEATEN/STABBED":
+			return "Beaten/Stabbed";
+			break;
+		case "CHILD ABUSE":
+			return "Child Abuse";
+			break;
+		case "MULTIPLE INJURIES":
+			return "Multiple Injuries";
+			break;
+		case "SHOT":
+			return "Shot";
+			break;
+		case "STABBING":
+			return "Stabbing";
+			break;
+		case "STRUCK BY AUTO":
+			return "Struck by Auto";
+			break;
+		case "THROWN FROM AUTO":
+			return "Thrown from Auto";
+			break;
+		case "UNKNOWN":
+			return "Unknown";
+			break;
+		case "VEHICLE":
+			return "Vehicle";
+			break;
+
+	}
+	return att;
 }
 
 fs.readFile(inputPath, 'utf-8', (err, data) => {
@@ -49,6 +141,7 @@ fs.readFile(inputPath, 'utf-8', (err, data) => {
 	// Put our buttons into some HTML architecture 
 	attributes.forEach(att => {
 		let filterGroup = `<div class='filters__group filters__group--${att.toLowerCase()}'>`;
+		filterGroup += `<p class='filters__group-label'>${labelFormatters.MAIN(att)}</p>`
 		filterGroup += buttons[att];
 		filterGroup += `</div>`;
 
